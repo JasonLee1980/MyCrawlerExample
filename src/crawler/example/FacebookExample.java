c60506a5b0456cb44ac440a11006929d64a89a2f
JasonLee1980 <jasonlee690910@gmail.com>
2017-09-02 15:46:07 +0800

    Practice for

--- src/crawler/example/FacebookExample.java
+++ src/crawler/example/FacebookExample.java
@@ -25,9 +25,9 @@ public class FacebookExample {
 		// [query sample]
 		// search?fields=name,id,likes,talking_about_count&limit=1000&q=靠北&type=page
 		String uri = 
-				"https://graph.facebook.com/v2.5"
-				+ "/search?q=%E9%9D%A0%E5%8C%97&type=page&limit=1000&fields=name,id,likes,talking_about_count"
-				+ "&access_token=EAACEdEose0cBAMzLyMlMxZAY73J1TBiZAHJhrF8du8fLhMmGYrUA3W9ezJDQk9oZAa07o4F2drZAL2k1cCtgdpl5LZClZCo6733A3xddNbjSAoTxEZBWY4rKnzLWTU3I0KMgT0qsw6eIRfdzg9RZCIkuTZCtGZCyzct657fywQpsFCNAZDZD";
+				"https://graph.facebook.com/v2.10"
+				+ "/search?q=%E5%9C%98%E8%B3%BC&type=page&fields=name,id,talking_about_count,fan_count"
+				+ "&access_token=EAACEdEose0cBAIJXVXfIL3AtJxdyVTCqYssm7cO3ruZCcJEjAsKZA052alFwqIgvW8ETOxNzLosBCZAmWUZBDs7Bu11c5TfD7vfDXM3rOYp5dNyKVFmQL9UJR8TN1OLmoZAQwwP8RumzdMJGOa3NKMzXGqEYikXnZAbVzJcONf71S4cOpoNYobGN9X7jx7d59IquZCs9nDZB1QZDZD";
 
 
 
